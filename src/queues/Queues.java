c266305249b10c99e09b9be445b4a2235239ed3c
PeterStanger92020362 <psta191@mywhitecliffe.com>
2020-06-13 13:48:13 +1200

    Finished iceCreamQueue task

--- src/queues/Queues.java
+++ src/queues/Queues.java
@@ -5,6 +5,11 @@
  */
 package queues;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  *
  * @author Peter
@@ -14,8 +19,74 @@ public class Queues {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws InterruptedException {
+        
+        /*
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for (int i = 1; i <= 10; i++) {
+            queue.add(i); 
+        }
+        
+        System.out.println("Element in the queue: "+queue);
+        int removed = queue.remove();
+        System.out.println(removed + " was removed");
+        
+        int top = queue.peek();
+        System.out.println("top element is: " + top);
+        
+        System.out.println(queue);
+        
+        System.out.println(queue.poll());
+        
+        System.out.println(queue.element());
+        
+        System.out.println(queue.offer(44));
+        
+        System.out.println(queue);
+
+        */
+        
+        //IceCreamQueue
+        
+       
+        Queue<String> customerQ = new LinkedList<>();
+        
+        customerQ.add("Simon");
+        customerQ.add("Steve");
+        customerQ.add("Sean");
+        customerQ.add("Samantha");
+        customerQ.add("Stella");
+        customerQ.add("Stanley");
+        customerQ.add("Sara");
+        
+        
+        
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+           @Override
+           public void run() {
+               
+               if(! customerQ.isEmpty()){
+                   System.out.println("The customer named " + customerQ.remove() + " has been served ice cream.");
+                   if (customerQ.peek() == null){
+                       System.out.println("That was the last customer in the queue, good job!");
+                   }
+               } else {
+                   System.out.println("\nThere are no customers left in the queue...");
+                   System.out.println("Goodbye...");
+                   timer.cancel();
+               }
+               
+           };
+           };
+        
+        timer.schedule( task,0, 4000);
+        //Thread.sleep(100000);
+           
     }
-    
+        
 }
+    
+
